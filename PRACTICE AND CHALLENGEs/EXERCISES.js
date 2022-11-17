79515921012d2e774b669b72f811128eff9c2682
DiegoFranco92 <diegofranco.daft@gmail.com>
2022-11-17 12:11:15 -0600

    update 17/11/22

--- PRACTICE AND CHALLENGEs/EXERCISES.js
+++ PRACTICE AND CHALLENGEs/EXERCISES.js
@@ -53,3 +53,36 @@ for (let i = 9; i<= 90; i++) {
 //crea una funcion que reciba un numero y imprima su tabla del 10: NOT FINISHED
 
 function multiplos(num) 
+
+
+
+
+// Funcion "soloNumeros" saca los numeros de un array:
+
+prueba = [1,2,"hola","perro",3,4,7];
+
+function soloNumeros(array) {
+    let output = [];
+    array.forEach (e => {if (typeof(e)==="number"){output.push(e)}})
+    return output;
+ }
+
+
+soloNumeros([1,2,7,"jesus","miguel", null])
+
+console.log(soloNumeros(prueba));
+
+//ANATOMY 
+function onlyNumbers(arr) {
+    //  We create an array to output
+    let output = [];
+    //  We use forEach() to check if the type of every element is the string 'number'
+    //  because we know that it outputs a lowercase string with the type.
+    //  If true, we push the element to the output.
+    arr.forEach(e => { if (typeof (e) === 'number') { output.push(e) } })
+    return output
+}
+
+console.log(onlyNumbers(array));
+
+//  Wich outputs [ 1 , 2 , 3 ]
