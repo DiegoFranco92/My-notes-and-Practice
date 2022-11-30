0f31fe7cd16c5049fb4b6c5c57e7d363d211e36f
DiegoFranco92 <diegofranco.daft@gmail.com>
2022-11-30 12:15:32 -0600

    update 30/11/22

---
+++ BACK-END STUFF/nodejs.js
@@ -0,0 +1,5 @@
+//WHAT IS NODE.JS?
+//is an open source and cross plataform for executing JS outside of a browser
+//we use Node to build back-end services also called APIs
+
+//API =  Application Programming Interface
