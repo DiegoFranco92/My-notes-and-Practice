0bd8b06123f86a2610bff82eaa6350abe68d4435
DiegoFranco92 <diegofranco.daft@gmail.com>
2022-11-30 15:51:01 -0600

    update 30/11/22

---
+++ BACK-END STUFF/nodejs Introduction.js
@@ -0,0 +1,29 @@
+//https://www.youtube.com/watch?v=TlB_eWDSMt4
+
+//WHAT IS NODE.JS?
+//is an open source and cross plataform for executing JS outside of a browser
+//we use Node to build back-end services also called APIs
+
+//API =  Application Programming Interface
+// this are the services that power client applications
+// like a webapp running on a web browser or a mobile app running in a mobile device
+//NODE.js is idea for highly-scalable, data-intesive and real-time back-end services
+//Paypal, Netflix, Uber and wallmart use NODE 
+
+
+//HOW DOES NODE WORK?
+//asynchronus requests 
+
+//GLOBAL OBJECT
+console.log(''); // is a global object NODE and JS
+setTimeout() // also a global object in NODE and JS
+
+// there are global object for NODE only
+// we have access to all thes global node objects with the "global" object
+global
+global.setTimeout
+
+
+//MODULES
+//every file in a NODE application, is consider a module
+// all the variables and functions defined in this module or file, is scoped to this file only
--- BACK-END STUFF/nodejs.js
+++
@@ -1,5 +0,0 @@
-//WHAT IS NODE.JS?
-//is an open source and cross plataform for executing JS outside of a browser
-//we use Node to build back-end services also called APIs
-
-//API =  Application Programming Interface
